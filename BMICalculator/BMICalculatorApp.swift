9664bce57bde63650072cf9087b094ca6947355d
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-10-13 19:30:18 +0530

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Thushini Abeysuriya on 2024-10-13.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
